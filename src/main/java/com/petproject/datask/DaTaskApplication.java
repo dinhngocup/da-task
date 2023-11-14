7a247595ab413dc3bf475f850745f4aa2c6c05d4
Phuong Dinh <phuongdinh1802@gmail.com>
2023-11-15 00:47:55 +0700

    Add Docker Compose file

--- src/main/java/com/petproject/datask/DaTaskApplication.java
+++ src/main/java/com/petproject/datask/DaTaskApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.petproject.datask.repository")
 public class DaTaskApplication {
 
     public static void main(String[] args) {
